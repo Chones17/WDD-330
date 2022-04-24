a287c87c99767458549f73d6694586c3c9a47480
Chones17 <chones17@byui.edu>
2022-04-23 21:52:43 -0600

    Update main.js
    Correct constant in main.js

--- js/main.js
+++ js/main.js
@@ -9,12 +9,12 @@ const links = [
       const ol = document.querySelector("#linkslist");
 
       links.forEach( link => {
-          const link = document.createElement("li");
+          const li = document.createElement("li");
           const href = document.createElement("a");
-          href.setAttribute("href", link.url);
-          href.innerText = link.label;
+          href.setAttribute("href", li.url);
+          href.innerText = li.label;
 
-          link.appendChild(href);
-          ol.appendChild(link);
+          li.appendChild(href);
+          ol.appendChild(li);
       })
   }
